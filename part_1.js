019a97c830ad238f9a62b2ca1c83ab1d5587c348
braydencorwin <braydentcorwin@gmail.com>
2022-11-14 21:19:20 -0500

    Update part_1.js

--- part_1.js
+++ part_1.js
@@ -8,35 +8,42 @@ console.log(board)
 
 const shipPlacement = [];
 let hitCount = 0;
-let winFlag = 'False'
+let winFlag = 'False';
 
 const shipPlacer = function(board){
-    let i = Math.floor(Math.random() * board.length());
-    shipPlacement.append(board[i]);
+    let i = Math.floor(Math.random()*board.length);
+    shipPlacement.push(board[i]);
 };
 
-while (winFlag = 'False'){
-    const fire = function(){
-    var fired = rs.keyInSelect(board, 'Where would you like to fire?')
+shipPlacer(board);
+
+let fired = rs.question(board, 'Where would you like to fire?')
+
+const fire = function(){
     let index = board.findIndex(x => x === fired);
     board.slice[index];
-    if (fired in shipPlacement){
-        console.log('HIT!')
-        hitCount +=1
-        }
+if (fired in shipPlacement){
+    console.log('HIT!')
+    hitCount +=1
+    } else{
+        console.log('Miss')
     }
+}
 
 const winCheck = function(counter) {
-    if (counter = 2){
+    if (counter === 2){
         console.log('You sank my battleships!')
         let winFlag = 'True'
     } else {
         let winFlag = 'False'
         }
-    }   
+}   
+
+for (; hitCount<2;) {
+    
+    fire();
+    winCheck(hitCount);
+    console.log(hitCount);
 }
+
 console.log(board);
-
-
-
-
